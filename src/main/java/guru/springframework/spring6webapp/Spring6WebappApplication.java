6e9f95be19921dabb00190b16a1aaed6a47da921
roybatista@gmail.com <adalmastabertoliliano@gmail.com>
2024-04-19 17:15:19 +0100

    Add Classes Author and Book

--- src/main/java/guru/springframework/spring6webapp/Spring6WebappApplication.java
+++ src/main/java/guru/springframework/spring6webapp/Spring6WebappApplication.java
@@ -8,7 +8,6 @@ public class Spring6WebappApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Spring6WebappApplication.class, args);
-        System.out.println("teste");
     }
 
 }
